1429c19d9ea9abb3da370c6635261600f1fd9c08
DESKTOP-QJLV6ND\Jeongsuhwan01 <suhwan07073232@gmail.com>
2024-02-27 12:43:28 +0900

    [24.02.27] 12:42 movie24 첫 커밋 index.html 및 resource 파일들 커밋 실행테스트 x

---
+++ src/main/java/project/movie24/Movie24Application.java
@@ -0,0 +1,13 @@
+package project.movie24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Movie24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Movie24Application.class, args);
+	}
+
+}
