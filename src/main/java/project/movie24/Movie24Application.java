b9d268a1758ed953f990b9af3fe3a30937f9e5b7
DESKTOP-QJLV6ND\Jeongsuhwan01 <suhwan07073232@gmail.com>
2024-03-01 23:03:53 +0900

    :fire: [24.03.01] 23:25 movie24 commit home.html(메인페이지) css적용 및 레이아웃 확인 mysql jpa연동 후 오류없이 실행 확인

--- src/main/java/project/movie24/Movie24Application.java
+++ src/main/java/project/movie24/Movie24Application.java
@@ -2,6 +2,13 @@ package project.movie24;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 @SpringBootApplication
 public class Movie24Application {
@@ -9,5 +16,4 @@ public class Movie24Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Movie24Application.class, args);
 	}
-
 }
---
+++ src/main/resources/static/resources/js/nav.js
@@ -0,0 +1,140 @@
+document.querySelectorAll(".popularPoster").forEach(function(item,index) {
+    item.addEventListener("mouseenter", () => {
+// 					item.nextElementSilbing.style.display = "block";
+        document.querySelector("#lookDetail"+(index+1)).style.display="block";
+        item.style.opacity = 0.4;
+        item.style.border = "2px solid #999797";
+    })
+});
+document.querySelectorAll(".popularPoster").forEach(function(item, index) {
+    item.addEventListener("mouseleave", () => {
+        document.querySelector("#lookDetail"+(index+1)).style.display="none";
+        item.style.opacity = 1;
+        item.style.border = "none";
+    })
+});
+document.querySelectorAll(".lookDetail").forEach(function(item,index) {
+    item.addEventListener("mouseenter", () => {
+        item.style.display="block";
+        document.getElementById('mainImg'+(index+1)).style.opacity = 0.4;
+        document.getElementById('mainImg'+(index+1)).style.border = "3px solid #999797";
+    })
+});
+////////네비게이터 수정중
+
+document.addEventListener("DOMContentLoaded", function() {
+    const liElements = document.querySelectorAll('nav ul li');
+    const navArea = document.getElementById('navArea');
+
+    let hoverIntent = false;
+
+    liElements.forEach(li => {
+        li.addEventListener('mouseenter', () => {
+            hoverIntent = true;
+            navArea.classList.add('show');
+            navArea.style.zIndex = 2;
+        });
+
+        li.addEventListener('mouseleave', () => {
+            hoverIntent = false;
+            setTimeout(() => {
+                if (!hoverIntent) {
+                    navArea.classList.remove('show');
+                }
+                // navArea.style.zIndex = -1;
+            }, ); // Add a delay before hiding to allow time for moving to menu_text
+        });
+    });
+
+    navArea.addEventListener('mouseenter', () => {
+        hoverIntent = true;
+    });
+
+    navArea.addEventListener('mouseleave', () => {
+        hoverIntent = false;
+        setTimeout(() => {
+            if (!hoverIntent) {
+                navArea.classList.remove('show');
+            }
+            // navArea.style.zIndex = -1;
+        }, ); // Add a delay before hiding to allow time for moving to menu_text
+    });
+});
+
+
+// -----------------------test
+var video1 = document.querySelector("#trailer-video1");
+var video2 = document.querySelector("#trailer-video2");
+
+const showTrailer = () =>{
+    video2.style.display = "none";
+}
+showTrailer();
+
+function playPause() {
+    if (video1.paused) {
+        video1.play();
+    } else {
+        video1.pause();
+    }
+    if (video2.paused) {
+        video2.play();
+    } else {
+        video2.pause();
+    }
+}
+
+function nextVideo(){
+
+    if(video1.style.display == "none"){
+        video1.style.display = "";
+        video2.style.display = "none";
+        document.querySelector("#h1-1").style.display = "block";
+        document.querySelector("#h2-1").style.display = "block";
+        document.querySelector("#h1-2").style.display = "none";
+        document.querySelector("#h2-2").style.display = "none";
+    }
+    else if(video2.style.display == "none"){
+        video1.style.display = "none";
+        video2.style.display = "";
+        document.querySelector("#h1-1").style.display = "none";
+        document.querySelector("#h2-1").style.display = "none";
+        document.querySelector("#h1-2").style.display = "block";
+        document.querySelector("#h2-2").style.display = "block";
+    }
+};
+
+var p1 = document.getElementsByClassName("popular");
+var p2 = document.getElementsByClassName("popular2");
+function showPopular(){
+    document.querySelector(".popularBtn").style.fontWeight = "900";
+    document.querySelector(".popularBtn").style.textDecoration = "underline";
+    document.querySelector(".trailerMovieBtn").style.fontWeight = "500";
+    document.querySelector(".trailerMovieBtn").style.textDecoration = "none";
+    document.querySelector(".popular2").style.display = "none";
+    document.querySelector(".popular").style.display = "block";
+};
+function showComing(){
+    document.querySelector(".trailerMovieBtn").style.fontWeight = "900";
+    document.querySelector(".trailerMovieBtn").style.textDecoration = "underline";
+    document.querySelector(".popularBtn").style.fontWeight = "500";
+    document.querySelector(".popularBtn").style.textDecoration = "none";
+    document.querySelector(".popular").style.display = "none";
+    document.querySelector(".popular2").style.display = "block";
+};
+function myCheck(){
+    const memberId = "${memberId}";
+    if(memberId === ""){
+        alert("로그인 후 이용가능한 기능입니다.");
+        if(confirm("로그인 페이지로 이동하시겠습니까?")){
+            location.href="/member/login.do";
+        }
+    }else{
+        modal.style.display = "block";
+    }
+}
+function outCheck(){
+    if(confirm("로그아웃 하시겠습니까?")){
+        location.href="/member/logout.do";
+    }
+}
